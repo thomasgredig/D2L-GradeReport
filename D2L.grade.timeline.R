4c2601e7ee607d71cff0df17adc6e4a4b94fa882
Thomas Gredig <thomas.gredig@csulb.edu>
2019-06-04 11:59:10 -0700

    add general functions

--- D2L.grade.timeline.R
+++ D2L.grade.timeline.R
@@ -10,38 +10,12 @@
 
 library(ggplot2)
 source('config.R')
+for(q1 in dir('R', pattern='[^(main)].*\\.R$')) { source(file.path('R',q1)) }
 
-result = data.frame()
-for(fname in file.list) {
-  read.csv(fname) -> data
-  names(data)
-  which(names(data)=='Calculated.Final.Grade.Numerator' |
-        names(data)=='Calculated.Final.Grade.Denominator') -> q
-  d <- data[,c(1,2,3,q)]
-  names(d)[4] = 'points'
-  names(d)[5] = 'total'
-  d$grade = d$points / max(d$points)
-  
-  # get date
-  regexpr('xport(.*)\\.',fname) -> m
-  regmatches(fname, m) -> m2
-  substr(m2, 7, nchar(m2)-1) -> full.date
-  substr(full.date, 6,10) -> short.date
-  d$date = short.date
-  d$full.date = full.date
-  print(full.date)
-  rbind(result,d[,c(1,2,3,7,6)]) -> result
-}
+file.list = find.fileList(path.source)
+file.latest = get.latestFile(file.list)
+result = read.grades(file.list)
 
-result$data = factor(result$date)
-result$name = paste(result$First.Name,result$Last.Name)
-str(result)
-# ggplot(result, aes(name, grade, color=date)) + geom_point()
-
-# creating title
-as.Date(result$date,"%m-%d") -> result$day
-result$lname = paste0(substr(result$First.Name,1,1),substr(result$Last.Name,1,5))
-result$lname2 = paste0(substr(result$First.Name,1,5),substr(result$Last.Name,1,1))
 
 
 ggplot(result, aes(day, grade)) + geom_line() + facet_wrap( ~ lname, ncol=10) +
--- config.R
+++ config.R
@@ -25,10 +25,4 @@ FILE.grade.timeline.top20 = file.path(path.results,'D2L.grade.timeline.FACETS.to
 FILE.grade.timeline.middle = file.path(path.results,'D2L.grade.timeline.FACETS.middle.png')
 FILE.grade.timeline.bottom20 = file.path(path.results,'D2L.grade.timeline.FACETS.bottom20.png')
 
-# make a list of gradebook files, including the latest file
-file.list = file.path(path.source,dir(path.source,pattern='-\\d{2}\\.csv$'))
-if (length(file.list)==0) {
-  print("ERROR: Grade book file list is empty.")
-}
-file.latest = file.list[which.max(file.info(file.list)$mtime)]
 
